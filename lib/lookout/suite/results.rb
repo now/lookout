8100f151b2250f2c8981232c27a9982278ccfb09
Nikolai Weibull <now@bitwi.se>
2010-03-18 11:00:21 +0100

    Use consistent style

--- lib/lookout/suite/results.rb
+++ lib/lookout/suite/results.rb
@@ -11,19 +11,19 @@ class Lookout::Suite::Results
   end
 
   def succeeded?
-    expectations.all? { |expectation| expectation.fulfilled? }
+    expectations.all?{ |expectation| expectation.fulfilled? }
   end
 
   def fulfillments
-    expectations.select { |expectation| expectation.fulfilled? }
+    expectations.select{ |expectation| expectation.fulfilled? }
   end
 
   def errors
-    expectations.select { |expectation| expectation.error? }
+    expectations.select{ |expectation| expectation.error? }
   end
 
   def failures
-    expectations.select { |expectation| expectation.failure? }
+    expectations.select{ |expectation| expectation.failure? }
   end
 
   attr_reader :expectations
