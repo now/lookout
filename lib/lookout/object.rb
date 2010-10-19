ce9b6a2ef652c0941a395cefc30802617dd9be95
Nikolai Weibull <now@bitwi.se>
2010-10-19 15:23:07 +0200

    Remove #instance_exec definition
    We only support 1.8.7 and above, so defining #instance_exec is
    pointless.

--- lib/lookout/object.rb
+++ lib/lookout/object.rb
@@ -8,26 +8,4 @@ class Object
   def not
     Lookout::Not.new(self)
   end
-
-  unless defined? instance_exec
-    InstanceExecMethods = Module.new
-    include InstanceExecMethods
-
-    def instance_exec(*args, &block)
-      begin
-        saved_critical, Thread.critical = Thread.critical, true
-        n = 0
-        n += 1 while respond_to?(name = "__instance_exec#{n}")
-        InstanceExecMethods.module_eval{ define_method name, &block }
-      ensure
-        Thread.critical = saved_critical
-      end
-
-      begin
-        send(name, *args)
-      ensure
-        InstanceExecMethods.module_eval{ remove_method name } rescue nil
-      end
-    end
-  end
 end
