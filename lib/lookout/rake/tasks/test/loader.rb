01cc39f07a046a9f68d0f3800f83914d5c360cc7
Nikolai Weibull <now@bitwi.se>
2010-11-17 14:07:56 +0100

    Deal with SyntaxError in rake loader

--- lib/lookout/rake/tasks/test/loader.rb
+++ lib/lookout/rake/tasks/test/loader.rb
@@ -3,9 +3,14 @@
 require 'lookout'
 
 ARGV.each do |arg|
-  if arg =~ /-r(.*)/
-    require $1
-  else
-    load arg
+  begin
+    if arg =~ /-r(.*)/
+      require $1
+    else
+      load arg
+    end
+  rescue SyntaxError => e
+    raise e unless matches = %r{\A(.*?:\d+): (.*)}m.match(e.message)
+    raise SyntaxError, matches[2], [matches[1]]
   end
 end
