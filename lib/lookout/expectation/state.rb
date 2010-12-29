830980112ecdf9ab98c3e7df265ce68ba41dfda4
Nikolai Weibull <now@bitwi.se>
2010-12-29 16:35:05 +0100

    Simplify exception handling in State

--- lib/lookout/expectation/state.rb
+++ lib/lookout/expectation/state.rb
@@ -8,22 +8,22 @@ class Lookout::Expectation::State
   rescue Exception => e
     return check(e) if StandardError === @expected and @expected.class == e.class
     return Lookout::Results::Fulfilled.new(file, line) if @expected.eql? e.class
-    if Class === @expected and StandardError >= @expected
-      Lookout::Results::Error.new(file, line, Lookout::Equality.message(@expected, e.class), e)
-    elsif StandardError === @expected
-      Lookout::Results::Error.new(file, line, Lookout::Equality.message(@expected.class, e.class), e)
-    else
-      Lookout::Results::Error.new(file, line, nil, e)
-    end
+    Lookout::Results::Error.new(file, line,
+      if Class === @expected and StandardError >= @expected
+        Lookout::Equality.message(@expected, e.class)
+      elsif StandardError === @expected
+        Lookout::Equality.message(@expected.class, e.class)
+      else
+        nil
+      end,
+      e)
   end
 
 private
 
   def check(actual)
-    if @expected == actual or Lookout::Equality.equal? @expected, actual
-      Lookout::Results::Fulfilled.new(file, line)
-    else
+    (@expected == actual or Lookout::Equality.equal? @expected, actual) ?
+      Lookout::Results::Fulfilled.new(file, line) :
       Lookout::Results::Failures::State.new(file, line, Lookout::Equality.message(@expected, actual))
-    end
   end
 end
