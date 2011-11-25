9796f3d5a6b07ed03cb1e7c2d131429d86233a45
Nikolai Weibull <now@bitwi.se>
2011-11-25 19:02:29 +0100

    Add some more tests for stubs

--- test/unit/lookout/stub/method.rb
+++ test/unit/lookout/stub/method.rb
@@ -31,6 +31,21 @@ Expectations do
     n
   end
 
+  expect 6 do
+    n = 0
+    Object.new.tap{ |o|
+      Lookout::Stub::Method.new(o, :a).yield(1, 2, 3).define
+      3.times{ o.a{ |i| n += i } }
+    }
+    n
+  end
+
+  expect 6 do
+    n = 0
+    Object.new.tap{ |o| Lookout::Stub::Method.new(o, :a).yield([1, 2, 3]).define }.a{ |i, j, k| n += i; n += j; n += k }
+    n
+  end
+
   expect StandardError do
     Object.new.tap{ |o| Lookout::Stub::Method.new(o, :a){ raise StandardError }.define }.a
   end
@@ -41,8 +56,14 @@ Expectations do
   end
 
   expect 1 do
-    a = nil
-    Object.new.tap{ |o| Lookout::Stub::Method.new(o, :a).yield(1).define }.a{ |v| a = v }
-    a
+    n = 0
+    Object.new.tap{ |o| Lookout::Stub::Method.new(o, :each).each(1).define }.each{ |i| n += i }
+    n
+  end
+
+  expect 6 do
+    n = 0
+    Object.new.tap{ |o| Lookout::Stub::Method.new(o, :each).each(1, 2, 3).define }.each{ |i| n += i }
+    n
   end
 end
