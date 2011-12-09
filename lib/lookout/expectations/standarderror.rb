5947d572c6a20776b882b1c05a6c42a876101a65
Nikolai Weibull <now@bitwi.se>
2011-12-09 11:51:26 +0100

    Rework Expectations::StandardError to be a bit more robust
    We don’t want to capture errors generated in #check, so move it outside
    of the rescue clause.

--- lib/lookout/expectations/standarderror.rb
+++ lib/lookout/expectations/standarderror.rb
@@ -3,18 +3,20 @@
 class Lookout::Expectations::StandardError < Lookout::Expectations::Object
   def evaluate
     begin
-      # TODO: Should we perhaps return an error if an error isn’t raised?  Do we
-      # want to be able to return an error and verify that it’s returned?
-      check(@block ? Context.new(subject, &@block).evaluate : false)
+      result = evaluate_in_context
     rescue Exception => actual
       # TODO: Guard against @expected#class, @expected#class#==, or actual#class failing.
-      return check(actual) if subject.class == actual.class
-      Lookout::Results::Error.
-        new(file,
-            line,
-            subject.class.to_lookout_expected.message(actual.class),
-            actual)
+      return subject.class == actual.class ?
+        check(actual) :
+        Lookout::Results::Error.
+          new(file,
+              line,
+              subject.class.to_lookout_expected.message(actual.class),
+              actual)
     end
+    # TODO: Should we perhaps return an error if an error isn’t raised?  Do we
+    # want to be able to return an error and verify that it’s returned?
+    check(result)
   rescue Exception => e
     Lookout::Results::Error.new(file, line, nil, e)
   end
