6bb9499243aa1c7cb7d3c37cdd572cf8f9a20ec7
Nikolai Weibull <now@bitwi.se>
2011-12-09 12:06:23 +0100

    Rework Expectations::Classes::StandardError in a similar manner

--- lib/lookout/expectations/classes/standarderror.rb
+++ lib/lookout/expectations/classes/standarderror.rb
@@ -3,15 +3,16 @@
 class Lookout::Expectations::Classes::StandardError < Lookout::Expectations::Object
   def evaluate
     begin
-      # TODO: Should we perhaps return an error if an error isn’t raised?  Do we
-      # want to be able to return an error and verify that it’s returned?
-      check(@block ? Context.new(subject, &@block).evaluate : false)
+      result = evaluate_in_context
     rescue subject
-      Lookout::Results::Fulfilled.new(file, line)
+      return Lookout::Results::Fulfilled.new(file, line)
     rescue Exception => e
       # TODO: Guard against failing e#class?
-      Lookout::Results::Error.new(file, line, @expected.message(e.class), e)
+      return Lookout::Results::Error.new(file, line, @expected.message(e.class), e)
     end
+    # TODO: Should we perhaps return an error if an error isn’t raised?  Do we
+    # want to be able to return an error and verify that it’s returned?
+    check(result)
   rescue Exception => e
     Lookout::Results::Error.new(file, line, nil, e)
   end
