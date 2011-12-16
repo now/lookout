adfd00ff8919f10128d5ab67aedb43ece31ed54b
Nikolai Weibull <now@bitwi.se>
2011-12-17 00:33:26 +0100

    Remove unnecessary parentheses

--- lib/lookout/expect.rb
+++ lib/lookout/expect.rb
@@ -3,11 +3,11 @@
 module Lookout::Expect
   class << self
     def actualize(subject, file, line, &block)
-      (begin
-         subject.to_lookout_expected
-       rescue NoMethodError
-         Lookout::Expected::Object.new(subject)
-       end).actualize(file, line, &block)
+      begin
+        subject.to_lookout_expected
+      rescue NoMethodError
+        Lookout::Expected::Object.new(subject)
+      end.actualize(file, line, &block)
     end
   end
 end
