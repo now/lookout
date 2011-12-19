1d0ee32331a282bc08ba5e0aa7054958ffaddbbe
Nikolai Weibull <now@bitwi.se>
2011-12-19 20:50:29 +0100

    Clean up some tests

--- test/unit/lookout/mock/methods.rb
+++ test/unit/lookout/mock/methods.rb
@@ -3,12 +3,9 @@
 Expectations do
   expect RuntimeError.
     new('can only mock one method per expectation: mock either stub.a(*args, &block) or stub.b(*args, &block)') do
-    methods = Lookout::Mock::Methods.new
-    begin
+    Lookout::Mock::Methods.during do |methods|
       methods.define(stub, :a, Lookout::Mock::Method::Calls::Lower.new(:a, 1))
       methods.define(stub, :b, Lookout::Mock::Method::Calls::Lower.new(:b, 1))
-    ensure
-      methods.undefine
     end
   end
 
@@ -18,24 +15,24 @@ Expectations do
 
   expect 2 do
     s = stub(:a => 1)
-    Lookout::Mock::Methods.during do |mocks|
-      mocks.define(s, :a, Lookout::Mock::Method::Calls::Lower.new(:method, 1)){ 2 }
+    Lookout::Mock::Methods.during do |methods|
+      methods.define(s, :a, Lookout::Mock::Method::Calls::Lower.new(:method, 1)){ 2 }
       s.a
     end
   end
 
   expect NoMethodError do
     s = Object.new
-    Lookout::Mock::Methods.during do |mocks|
-      mocks.define(s, :a, Lookout::Mock::Method::Calls::Lower.new(:a, 1)){ 2 }
+    Lookout::Mock::Methods.during do |methods|
+      methods.define(s, :a, Lookout::Mock::Method::Calls::Lower.new(:a, 1)){ 2 }
     end
     s.a
   end
 
   expect 1 do
     s = stub(:a => 1)
-    Lookout::Mock::Methods.during do |mocks|
-      mocks.define(s, :a, Lookout::Mock::Method::Calls::Lower.new(:a, 1)){ 2 }
+    Lookout::Mock::Methods.during do |methods|
+      methods.define(s, :a, Lookout::Mock::Method::Calls::Lower.new(:a, 1)){ 2 }
     end
     s.a
   end
