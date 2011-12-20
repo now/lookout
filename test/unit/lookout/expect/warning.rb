2a629cbd38e82758ac6235838d40930fcf46425c
Nikolai Weibull <now@bitwi.se>
2011-12-20 15:14:11 +0100

    Improve tests for Expect::Warning

--- test/unit/lookout/expect/warning.rb
+++ test/unit/lookout/expect/warning.rb
@@ -1,20 +1,24 @@
 # -*- coding: utf-8 -*-
 
 Expectations do
+  expect Lookout::Expect::Warning do
+    Lookout::Warning.new('a').to_lookout_expected.actualize('test', 1)
+  end
+
   expect Lookout::Results::Fulfilled.new('test', 1) do
-    Lookout::Expect::Warning.new(Lookout::Expected::Warning.new(Lookout::Warning.new('a')), 'test', 1){ |io|
+    Lookout::Warning.new('a').to_lookout_expected.actualize('test', 1){
       warn 'a'
     }.call
   end
 
   expect Lookout::Results::Fulfilled.new('test', 1) do
-    Lookout::Expect::Warning.new(Lookout::Expected::Warning.new(Lookout::Warning.new('a')), 'test', 1){ |io|
+    Lookout::Warning.new('a').to_lookout_expected.actualize('test', 1){
       warn '%s:%d: warning: a' % [__FILE__, __LINE__]
     }.call
   end
 
   expect Lookout::Results::Failures::State.new('test', 1, 'warning("b")≠warning("a"): [-b-]{+a+}') do
-    Lookout::Expect::Warning.new(Lookout::Expected::Warning.new(Lookout::Warning.new('a')), 'test', 1){ |io|
+    Lookout::Warning.new('a').to_lookout_expected.actualize('test', 1){
       warn 'b'
     }.call
   end
@@ -22,7 +26,7 @@ Expectations do
   expect Lookout::Results::Failures::State.
     new('test', 1,
         'warning("%s:%d: warning: b")≠warning("a"): [-b-]{+a+}' % [__FILE__, __LINE__ + 2]) do
-    Lookout::Expect::Warning.new(Lookout::Expected::Warning.new(Lookout::Warning.new('a')), 'test', 1){ |io|
+    Lookout::Warning.new('a').to_lookout_expected.actualize('test', 1){
       warn '%s:%d: warning: b' % [__FILE__, __LINE__]
     }.call
   end
