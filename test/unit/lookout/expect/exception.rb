ee030d5ced854cc11a64bda3c1cc08f58d1ea792
Nikolai Weibull <now@bitwi.se>
2011-12-20 13:59:26 +0100

    Improve tests for Except::Exception

--- test/unit/lookout/expect/exception.rb
+++ test/unit/lookout/expect/exception.rb
@@ -1,21 +1,23 @@
 # -*- coding: utf-8 -*-
 
 Expectations do
+  expect Lookout::Expect::Exception do
+    RuntimeError.new('error').to_lookout_expected.actualize('test', 1)
+  end
+
   expect Lookout::Results::Fulfilled.new('test', 1) do
-    Lookout::Expect::Exception.
-      new(Lookout::Expected::Exception.
-          new(RuntimeError.new('error')), 'test', 1){ raise 'error' }.call
+    RuntimeError.new('error').to_lookout_expected.actualize('test', 1){ raise 'error' }.call
   end
 
   expect Lookout::Results::Failures::State.new('test', 1, '#<RuntimeError: b>≠#<RuntimeError: a>: [-b-]{+a+}') do
-    Lookout::Expect::Exception.
-      new(Lookout::Expected::Exception.
-          new(RuntimeError.new('a')), 'test', 1){ raise 'b' }.call
+    RuntimeError.new('a').to_lookout_expected.actualize('test', 1){ raise 'b' }.call
+  end
+
+  expect Lookout::Results::Error do
+    StandardError.new('a').to_lookout_expected.actualize('test', 1){ raise 'b' }.call
   end
 
   expect 'RuntimeError≠StandardError' do
-    Lookout::Expect::Exception.
-      new(Lookout::Expected::Exception.
-          new(StandardError.new('a')), 'test', 1){ raise 'b' }.call.message
+    StandardError.new('a').to_lookout_expected.actualize('test', 1){ raise 'b' }.call.message
   end
 end
