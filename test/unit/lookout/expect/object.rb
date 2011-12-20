1054d4f7be12e8746382469d93434fb2c16f902a
Nikolai Weibull <now@bitwi.se>
2011-12-20 14:59:23 +0100

    Add tests for Expect::Object#<=>

--- test/unit/lookout/expect/object.rb
+++ test/unit/lookout/expect/object.rb
@@ -12,4 +12,28 @@ Expectations do
   expect Lookout::Results::Failures::State.new('test', 1, '3≠2') do
     Lookout::Expect::Object.new(Lookout::Expected::Object.new(2), 'test', 1){ 3 }.call
   end
+
+  expect nil do
+    2.to_lookout_expected.actualize('test', 1) <=> 2
+  end
+
+  expect 0 do
+    2.to_lookout_expected.actualize('test', 1) <=> 2.to_lookout_expected.actualize('test', 1)
+  end
+
+  expect true do
+    (2.to_lookout_expected.actualize('test', 1) <=> 2.to_lookout_expected.actualize('test', 2)) < 0
+  end
+
+  expect false do
+    (2.to_lookout_expected.actualize('test', 1) <=> 2.to_lookout_expected.actualize('test', 0)) < 0
+  end
+
+  expect false do
+    (2.to_lookout_expected.actualize('test', 1) <=> 2.to_lookout_expected.actualize('test', 2)) > 0
+  end
+
+  expect true do
+    (2.to_lookout_expected.actualize('test', 1) <=> 2.to_lookout_expected.actualize('test', 0)) > 0
+  end
 end
