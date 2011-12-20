2f902c860f92c8aaa5d21f1a21c38600fa8fb783
Nikolai Weibull <now@bitwi.se>
2011-12-20 13:59:14 +0100

    Improve tests for Except::Classes::Exception

--- test/unit/lookout/expect/classes/exception.rb
+++ test/unit/lookout/expect/classes/exception.rb
@@ -1,31 +1,27 @@
 # -*- coding: utf-8 -*-
 
 Expectations do
+  expect Lookout::Expect::Classes::Exception do
+    SystemExit.to_lookout_expected.actualize('test', 1){ exit }
+  end
+
   expect Lookout::Results::Fulfilled.new('test', 1) do
     begin
-      Lookout::Expect::Classes::Exception.
-        new(Lookout::Expected::Classes::Exception.
-            new(SystemExit), 'test', 1){ exit }.call
+      SystemExit.to_lookout_expected.actualize('test', 1){ exit }.call
     rescue SystemExit
       raise 'SystemExit not caught'
     end
   end
 
   expect Lookout::Results::Fulfilled.new('test', 1) do
-    Lookout::Expect::Classes::Exception.
-      new(Lookout::Expected::Classes::Exception.
-          new(StandardError), 'test', 1){ raise 'error' }.call
+    StandardError.to_lookout_expected.actualize('test', 1){ raise 'error' }.call
   end
 
   expect Lookout::Results::Fulfilled.new('test', 1) do
-    Lookout::Expect::Classes::Exception.
-      new(Lookout::Expected::Classes::Exception.
-          new(RuntimeError), 'test', 1){ raise 'error' }.call
+    RuntimeError.to_lookout_expected.actualize('test', 1){ raise 'error' }.call
   end
 
   expect Lookout::Results::Error do
-    Lookout::Expect::Classes::Exception.
-      new(Lookout::Expected::Classes::Exception.
-          new(ArgumentError), 'test', 1){ raise 'error' }.call
+    ArgumentError.to_lookout_expected.actualize('test', 1){ raise 'error' }.call
   end
 end
