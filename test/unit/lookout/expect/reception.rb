f32b065b8aa5b5f26665c70848f9d136abe8eebb
Nikolai Weibull <now@bitwi.se>
2011-12-20 14:01:28 +0100

    Improve tests for Expect::Reception

--- test/unit/lookout/expect/reception.rb
+++ test/unit/lookout/expect/reception.rb
@@ -1,6 +1,10 @@
 # -*- coding: utf-8 -*-
 
 Expectations do
+  expect Lookout::Expect::Reception do
+    stub.to.receive.call.to_lookout_expected.actualize('test', 1)
+  end
+
   expect Lookout::Results::Fulfilled.new('test', 1) do
     stub.to.receive.call.to_lookout_expected.actualize('test', 1){ |o| o.call }.call
   end
