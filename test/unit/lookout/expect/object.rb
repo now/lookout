56ce16ca7859f92ad3e2d71d949001c178239919
Nikolai Weibull <now@bitwi.se>
2011-12-20 15:02:55 +0100

    Improve tests for Expect::Object

--- test/unit/lookout/expect/object.rb
+++ test/unit/lookout/expect/object.rb
@@ -2,15 +2,23 @@
 
 Expectations do
   expect Lookout::Results::Fulfilled.new('test', 1) do
-    Lookout::Expect::Object.new(Lookout::Expected::Object.new(2), 'test', 1){ 2 }.call
-  end
-
-  expect Lookout::Results::Error do
-    Lookout::Expect::Object.new(Lookout::Expected::Object.new(stub(:== => proc{ raise })), 'test', 1){ 2 }.call
+    2.to_lookout_expected.actualize('test', 1){ 2 }.call
   end
 
   expect Lookout::Results::Failures::State.new('test', 1, '3≠2') do
-    Lookout::Expect::Object.new(Lookout::Expected::Object.new(2), 'test', 1){ 3 }.call
+    2.to_lookout_expected.actualize('test', 1){ 3 }.call
+  end
+
+  expect Lookout::Results::Error do
+    stub(:== => proc{ raise 'error' }).to_lookout_expected.actualize('test', 1){ 2 }.call
+  end
+
+  expect 'error (RuntimeError)' do
+    stub(:== => proc{ raise 'error' }).to_lookout_expected.actualize('test', 1){ 2 }.call.exception.message
+  end
+
+  expect Comparable do
+    2.to_lookout_expected.actualize('test', 1)
   end
 
   expect nil do
