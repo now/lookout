2aef8e237aa880222f635e51e391c04dc2dd6c93
Nikolai Weibull <now@bitwi.se>
2011-12-27 14:57:16 +0100

    Conditionalize two more tests
    These tests depend on Ruby 1.9-specific String() rules

--- test/unit/lookout/inspect/error.rb
+++ test/unit/lookout/inspect/error.rb
@@ -5,15 +5,15 @@ Expectations do
     Lookout::Inspect::Error.new(RuntimeError.new('error')).call
   end
 
-  expect "cannot retrieve error message: error" do
-    Lookout::Inspect::Error.new(stub(:to_str => proc{ raise 'error' })).call
-  end
-
-  expect "cannot retrieve error message: can't convert Lookout::Stub::Object to String (Lookout::Stub::Object#to_s gives NilClass)" do
-    Lookout::Inspect::Error.new(stub(:to_str => nil, :to_s => nil)).call
-  end
-
   if ''.respond_to? :encode
+    expect "cannot retrieve error message: error" do
+      Lookout::Inspect::Error.new(stub(:to_str => proc{ raise 'error' })).call
+    end
+
+    expect "cannot retrieve error message: can't convert Lookout::Stub::Object to String (Lookout::Stub::Object#to_s gives NilClass)" do
+      Lookout::Inspect::Error.new(stub(:to_str => nil, :to_s => nil)).call
+    end
+
     expect 'cannot encode error message for output: "\xE2" from ASCII-8BIT to UTF-8; dumping instead: "can\xE2\x80\x99t"' do
       Lookout::Inspect::Error.new(stub(:to_str => 'can’t'.force_encoding('ASCII-8BIT'))).call
     end
