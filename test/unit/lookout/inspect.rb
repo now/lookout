8c306e14a2e8ec367598f0bd596298c49a284144
Nikolai Weibull <now@bitwi.se>
2011-12-27 14:43:00 +0100

    Conditionalize some 1.9-specific tests

--- test/unit/lookout/inspect.rb
+++ test/unit/lookout/inspect.rb
@@ -5,21 +5,23 @@ Expectations do
     Lookout::Inspect.new('v', 'value').call
   end
 
-  expect Encoding::UTF_8 do
-    Lookout::Inspect.new('v'.encode('ASCII-8BIT'), 'value').call.encoding
-  end
+  if ''.respond_to? :encode
+    expect Encoding::UTF_8 do
+      Lookout::Inspect.new('v'.encode('ASCII-8BIT'), 'value').call.encoding
+    end
 
-  expect '(cannot inspect value: error)' do
-    Lookout::Inspect.new(stub(:inspect => proc{ raise 'error' }), 'value').call
-  end
+    expect '(cannot inspect value: error)' do
+      Lookout::Inspect.new(stub(:inspect => proc{ raise 'error' }), 'value').call
+    end
 
-  expect '(cannot encode inspected value for output: "\xE2" from ASCII-8BIT to UTF-8; dumping instead: "can\xE2\x80\x99t")' do
-    Lookout::Inspect.new(stub(:inspect => 'can’t'.force_encoding('ASCII-8BIT')), 'value').call
-  end
+    expect '(cannot encode inspected value for output: "\xE2" from ASCII-8BIT to UTF-8; dumping instead: "can\xE2\x80\x99t")' do
+      Lookout::Inspect.new(stub(:inspect => 'can’t'.force_encoding('ASCII-8BIT')), 'value').call
+    end
 
-  expect '(cannot encode inspected value for output: "\xE2" from ASCII-8BIT to UTF-8; dumping also failed: error)' do
-    message = 'can’t'.force_encoding('ASCII-8BIT')
-    stub(message).dump{ raise 'error' }
-    Lookout::Inspect.new(stub(:inspect => message), 'value').call
+    expect '(cannot encode inspected value for output: "\xE2" from ASCII-8BIT to UTF-8; dumping also failed: error)' do
+      message = 'can’t'.force_encoding('ASCII-8BIT')
+      stub(message).dump{ raise 'error' }
+      Lookout::Inspect.new(stub(:inspect => message), 'value').call
+    end
   end
 end
