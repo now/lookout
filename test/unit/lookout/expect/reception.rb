49cefb561e601be7a0c5004777cca011583c1790
Nikolai Weibull <now@bitwi.se>
2011-12-27 14:51:07 +0100

    Conditionalize another test

--- test/unit/lookout/expect/reception.rb
+++ test/unit/lookout/expect/reception.rb
@@ -13,8 +13,14 @@ Expectations do
     stub.to.receive.call.to_lookout_expected.actualize('test', 1).call
   end
 
-  expect Lookout::Results::Error.new('test', 1, nil, RuntimeError.new('error').tap{ |e| e.set_backtrace([]) }) do
-    stub.to.receive.call.to_lookout_expected.actualize('test', 1){ raise RuntimeError, 'error', [] }.call
+  if RUBY_VERSION < '1.9'
+    expect Lookout::Results::Error do
+      stub.to.receive.call.to_lookout_expected.actualize('test', 1){ raise RuntimeError, 'error', [] }.call
+    end
+  else
+    expect Lookout::Results::Error.new('test', 1, nil, RuntimeError.new('error').tap{ |e| e.set_backtrace([]) }) do
+      stub.to.receive.call.to_lookout_expected.actualize('test', 1){ raise RuntimeError, 'error', [] }.call
+    end
   end
 
   expect Lookout::Results::Failures::Behavior.new('test', 1, 'expected stub.call(*args, &block) not to be called') do
