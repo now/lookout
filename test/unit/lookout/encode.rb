cbe33c299629461d3f9c0583f337a62d80eb00b3
Nikolai Weibull <now@bitwi.se>
2011-12-28 14:12:01 +0100

    Remove some version-specific tests

--- test/unit/lookout/encode.rb
+++ test/unit/lookout/encode.rb
@@ -1,19 +1,9 @@
 # -*- coding: utf-8 -*-
 
 Expectations do
-  expect RuntimeError.new('error') do
-    Lookout::Encode.new(stub(:to_str => proc{ raise 'error' })).call
-  end
-
   expect 'a' do
     a = 'a'
     class << a; undef encode end if a.respond_to? :encode
-    Lookout::Encode.new(a).call
-  end
-
-  expect 'a' do
-    a = 'a'
-    class << a; def respond_to?(m); raise 'error' end end
     Lookout::Encode.new(a).call
   end
 
