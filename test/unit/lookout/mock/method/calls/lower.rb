e29a1fff4259ee60708b1fcf70a53316e67285ab
Nikolai Weibull <now@bitwi.se>
2011-12-28 14:33:07 +0100

    Add a missing test

--- test/unit/lookout/mock/method/calls/lower.rb
+++ test/unit/lookout/mock/method/calls/lower.rb
@@ -56,4 +56,11 @@ Expectations do
   expect Lookout::Mock::Method::Calls::Lower.new(:method, 3) do |o|
     o.call.call.call.call.verify
   end
+
+  expect Lookout::Mock::Method::Calls::Error.new('expected method to be called at least 4 times, but was only called 3 times') do
+    Lookout::Mock::Method::Calls::Lower.new(:method, 4).call.call.call.verify
+  end
+  expect Lookout::Mock::Method::Calls::Lower.new(:method, 4) do |o|
+    o.call.call.call.call.verify
+  end
 end
