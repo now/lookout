e101b553430497d6c54244fae84761d9033c6065
Nikolai Weibull <now@bitwi.se>
2012-01-02 12:18:32 +0100

    Actually fix build on 1.8.7

--- test/unit/lookout/results/error.rb
+++ test/unit/lookout/results/error.rb
@@ -1,19 +1,23 @@
 # -*- coding: utf-8 -*-
 
 Expectations do
-  expect true do
-    Lookout::Results::Error.new('a', 1, nil, RuntimeError.new.tap{ |e| e.set_backtrace [] }) ==
-      Lookout::Results::Error.new('a', 1, nil, RuntimeError.new.tap{ |e| e.set_backtrace [] })
-  end
+  # In Ruby 1.8.7, exceptions can’t be compared properly.
+  if RuntimeError.new == RuntimeError.new
+    expect true do
+      Lookout::Results::Error.new('a', 1, nil, RuntimeError.new) == Lookout::Results::Error.new('a', 1, nil, RuntimeError.new)
+    end
 
-  expect false do
-    Lookout::Results::Error.new('a', 1, nil, RuntimeError.new.tap{ |e| e.set_backtrace [] }) ==
-      Lookout::Results::Error.new('a', 1, 'a', RuntimeError.new.tap{ |e| e.set_backtrace [] })
-  end
+    expect false do
+      Lookout::Results::Error.new('a', 1, nil, RuntimeError.new) == Lookout::Results::Error.new('a', 1, 'a', RuntimeError.new)
+    end
 
-  expect false do
-    Lookout::Results::Error.new('a', 1, nil, RuntimeError.new.tap{ |e| e.set_backtrace [] }) ==
-      Lookout::Results::Error.new('a', 1, nil, StandardError.new.tap{ |e| e.set_backtrace [] })
+    expect false do
+      Lookout::Results::Error.new('a', 1, nil, RuntimeError.new) == Lookout::Results::Error.new('a', 1, nil, StandardError.new)
+    end
+
+    expect Lookout::Results::Error::Exception.new(RuntimeError.new('error')) do
+      Lookout::Results::Error.new('test', 1, 'a≠b', RuntimeError.new('error')).exception
+    end
   end
 
   expect "test:1: error (RuntimeError)\n" do
