6646354ff25ccac1a98a16161b8f3d58adaf8632
Nikolai Weibull <now@bitwi.se>
2012-02-09 14:18:23 +0100

    Reverse order of #== and #=== tests for Module, Range, Regexp

--- lib/lookout/expected/range.rb
+++ lib/lookout/expected/range.rb
@@ -2,7 +2,7 @@
 
 class Lookout::Expected::Range < Lookout::Expected::Object
   def difference(other)
-    (subject === other or subject == other) ?
+    (subject == other or subject === other) ?
       nil :
       Lookout::Difference::Range.new(other, subject)
   end
--- lib/lookout/expected/regexp.rb
+++ lib/lookout/expected/regexp.rb
@@ -2,7 +2,7 @@
 
 class Lookout::Expected::Regexp < Lookout::Expected::Object
   def difference(other)
-    (subject === other or subject == other) ?
+    (subject == other or subject === other) ?
       nil :
       Lookout::Difference::Regexp.new(other, subject)
   end
