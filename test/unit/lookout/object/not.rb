03876e16bf4277203bb20c3bcfd366fd78965d9c
Nikolai Weibull <now@bitwi.se>
2012-03-19 09:21:30 +0100

    Add some missing tests

--- test/unit/lookout/object/not.rb
+++ test/unit/lookout/object/not.rb
@@ -1,4 +1,7 @@
 # -*- coding: utf-8 -*-
 
 Expectations do
+  expect true do
+    Lookout::Object::Not.new([]).to.receive == Lookout::Object::Not::Receive.new([])
+  end
 end
--- test/unit/lookout/object/to.rb
+++ test/unit/lookout/object/to.rb
@@ -1,4 +1,7 @@
 # -*- coding: utf-8 -*-
 
 Expectations do
+  expect true do
+    Lookout::Object::To.new([]).receive == Lookout::Object::To::Receive.new([])
+  end
 end
