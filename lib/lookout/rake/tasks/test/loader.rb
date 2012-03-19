0a0d108cc173cc1ea701ff062ca0d0546ba2af61
Nikolai Weibull <now@bitwi.se>
2012-03-19 22:26:36 +0100

    Separate out concerns slightly in loader

--- lib/lookout/rake/tasks/test/loader.rb
+++ lib/lookout/rake/tasks/test/loader.rb
@@ -2,9 +2,9 @@
 
 require 'lookout'
 
-failed = false
 line = nil
 only_load = false
+files = []
 pwd = Dir.pwd
 ARGV.each do |arg|
   if not only_load and arg == '--'
@@ -14,22 +14,27 @@ ARGV.each do |arg|
   elsif not only_load and arg =~ /\A-l(.*)/
     line = $1.to_i
   else
-    file = File.expand_path(arg, pwd)
-    expectations = Lookout::Expectations.new(file)
-    if line
-      target = nil.to_lookout_expected.expect(file, line){
-        raise RuntimeError, 'line expectation not found: %s:%d' % [arg, line]
-      }
-      [(expectations.take_while{ |expect| expect <= target }.last or target)]
-    else
-      expectations
-    end.each do |expect|
-      result = expect.call
-      next if Lookout::Results::Success === result
-      failed = true
-      message = result.to_s
-      $stderr.puts message.start_with?(pwd) ? message[pwd.length+1..-1] : message
-    end
+    files << [arg, File.expand_path(arg, pwd)]
   end
 end
+
+failed = false
+files.each do |file, expanded|
+  expectations = Lookout::Expectations.new(expanded)
+  if line
+    target = nil.to_lookout_expected.expect(expanded, line){
+      raise RuntimeError, 'line expectation not found: %s:%d' % [file, line]
+    }
+    [(expectations.take_while{ |expect| expect <= target }.last or target)]
+  else
+    expectations
+  end.each do |expect|
+    result = expect.call
+    next if Lookout::Results::Success === result
+    failed = true
+    message = result.to_s
+    $stderr.puts message.start_with?(pwd) ? message[pwd.length+1..-1] : message
+  end
+end
+
 exit false if failed
