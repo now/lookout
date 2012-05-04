241dae3a69684d75eb82bc6d3e571dcba2974a50
Nikolai Weibull <now@bitwi.se>
2012-05-04 13:29:20 +0200

    Fix documentation type

--- lib/lookout/expected/classes/exception.rb
+++ lib/lookout/expected/classes/exception.rb
@@ -5,9 +5,9 @@ class Lookout::Expected::Classes::Exception < Lookout::Expected::Object
   # @param (see Object#expect)
   # @yieldparam [Class] class
   # @yieldreturn [Class]
-  # @return [Lookout::Expect::Classes::Exception] An expect block for the
-  #   receiver at _line_ in _file_ that’ll yield the {#expected} exception
-  #   class and expect it or one of its descendants to be raised
+  # @return [Expect::Classes::Exception] An expect block for the receiver at
+  #   _line_ in _file_ that’ll yield the {#expected} exception class and expect
+  #   it or one of its descendants to be raised
   def expect(file, line, &block)
     Lookout::Expect::Classes::Exception.new(self, file, line, &block)
   end
