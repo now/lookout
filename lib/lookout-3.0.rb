4b8c45ad7cf1b05dd2e715983f327848c696875d
Nikolai Weibull <now@bitwi.se>
2012-05-04 14:13:05 +0200

    Add a note about plugins namespace

--- lib/lookout-3.0.rb
+++ lib/lookout-3.0.rb
@@ -24,6 +24,8 @@
 # There’s a command-line interface defined in {Interfaces::Commandline} that can
 # be used by other interfaces that want to invoke Lookout on a set of files.
 # It’s used by the {http://disu.se/software/lookout-rake Lookout-Rake} library.
+#
+# Plugins should place their namespace under the {Lookout::Plugins} namespace.
 module Lookout
   class << self
     def location(location)
